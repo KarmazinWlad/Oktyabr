ba76a7171551c1127df3f125792a4e74cb41bab4
RahatLukum13 <karmazin2007@mail.ru>
2025-01-01 19:46:35 +0300

    a

--- js/heart.js
+++ js/heart.js
@@ -1,6 +1,6 @@
 document.getElementsByClassName('favorite')[0].addEventListener('mouseover', () =>{
-    document.getElementsByClassName('favorite')[0].getElementsByTagName('img')[0].src="C:/Users/karma/Desktop/site/img/img1.png";
+    document.getElementsByClassName('favorite')[0].getElementsByTagName('img')[0].src="https://karmazinwlad.github.io/Oktyabr/img/img1.png";
 });
 document.getElementsByClassName('favorite')[0].addEventListener('mouseout', () =>{
-    document.getElementsByClassName('favorite')[0].getElementsByTagName('img')[0].src="C:/Users/karma/Desktop/site/img/img.png";
+    document.getElementsByClassName('favorite')[0].getElementsByTagName('img')[0].src="https://karmazinwlad.github.io/Oktyabr/img/img.png";
 });
