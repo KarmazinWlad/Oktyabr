57a13f7931a81dd4a5e5592e82082ceab9b2a819
RahatLukum13 <karmazin2007@mail.ru>
2025-01-01 23:59:51 +0300

    a

--- js/main.js
+++ js/main.js
@@ -8,21 +8,23 @@ for(const element of document.getElementsByClassName("heart")){
     }
 }
 if('ontouchstart' in window || navigator.maxTouchPoints){
-    window.addEventListener('touchcancel', () =>{
-        if(document.getElementById("11").clientHeight-document.getElementById("13").clientHeight-document.getElementById("14").clientHeight - 45 <= pageYOffset){
-            window.scrollTo(0, document.getElementById("13").clientHeight*2+75 + pageYOffset - (document.getElementById("11").clientHeight-document.getElementById("13").clientHeight-document.getElementById("14").clientHeight - 45));
+    window.addEventListener('scroll', () =>{
+        window.addEventListener('touchcancel'){
+            if(document.getElementById("11").clientHeight-document.getElementById("13").clientHeight-document.getElementById("14").clientHeight - 45 <= pageYOffset){
+                window.scrollTo(0, document.getElementById("13").clientHeight*2+75);
+            }
+            else if(document.getElementById("13").clientHeight+30 >= pageYOffset){
+                window.scrollTo(0, document.getElementById("11").clientHeight - document.getElementById("13").clientHeight*2-document.getElementById("14").clientHeight -90);
+            }
         }
-        else if(document.getElementById("13").clientHeight+30 >= pageYOffset){
-            window.scrollTo(0, document.getElementById("11").clientHeight - document.getElementById("13").clientHeight*2-document.getElementById("14").clientHeight -90 + pageYOffset - (document.getElementById("13").clientHeight+30));
-        }
-    });
-    window.addEventListener('touchend', () =>{
-        if(document.getElementById("11").clientHeight-document.getElementById("13").clientHeight-document.getElementById("14").clientHeight - 45 <= pageYOffset){
-            window.scrollTo(0, document.getElementById("13").clientHeight*2+75 + pageYOffset - (document.getElementById("11").clientHeight-document.getElementById("13").clientHeight-document.getElementById("14").clientHeight - 45));
-        }
-        else if(document.getElementById("13").clientHeight+30 >= pageYOffset){
-            window.scrollTo(0, document.getElementById("11").clientHeight - document.getElementById("13").clientHeight*2-document.getElementById("14").clientHeight -90 + pageYOffset - (document.getElementById("13").clientHeight+30));
-        }
+        window.addEventListener('touchend', () =>{
+            if(document.getElementById("11").clientHeight-document.getElementById("13").clientHeight-document.getElementById("14").clientHeight - 45 <= pageYOffset){
+                window.scrollTo(0, document.getElementById("13").clientHeight*2+75 + pageYOffset - (document.getElementById("11").clientHeight-document.getElementById("13").clientHeight-document.getElementById("14").clientHeight - 45));
+            }
+            else if(document.getElementById("13").clientHeight+30 >= pageYOffset){
+                window.scrollTo(0, document.getElementById("11").clientHeight - document.getElementById("13").clientHeight*2-document.getElementById("14").clientHeight -90 + pageYOffset - (document.getElementById("13").clientHeight+30));
+            }
+        });
     });
 }
 else{
