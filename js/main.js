46ab6ac3bbfc48c4528d6a661ae2ec0b5cb722aa
RahatLukum13 <karmazin2007@mail.ru>
2025-01-21 20:21:02 +0300

    a

--- js/main.js
+++ js/main.js
@@ -7,18 +7,18 @@ for(const element of document.getElementsByClassName("heart")){
         element.src="https://karmazinwlad.github.io/Oktyabr/img/img1.png";
     }
 }
-console.log(2);
+console.log(1);
 if('ontouchstart' in window || navigator.maxTouchPoints){
     var flag = true;
     function scrolling(){
         if(flag == true){
             if(document.getElementById("11").clientHeight - document.getElementById("13").clientHeight - document.getElementById("14").clientHeight - 45 <= pageYOffset){
-                window.scrollTo(0, document.getElementById("13").clientHeight * 2 + 75);
-                setTimeout(window.scrollTo({ top: document.getElementById("13").clientHeight * 2 + 175, left: 0, behavior: 'smooth' }),10);
+                window.scrollTo({ top: document.getElementById("13").clientHeight * 2 + 75, left: 0, behavior: 'instant' });
+                setTimeout(window.scrollTo({ top: document.getElementById("13").clientHeight * 2 + 175, left: 0, behavior: 'smooth' }), 100);
             }
             else if(document.getElementById("13").clientHeight + 30 >= pageYOffset){
-                window.scrollTo(0, document.getElementById("11").clientHeight - document.getElementById("13").clientHeight * 2 - document.getElementById("14").clientHeight - 90);
-                setTimeout(window.scrollTo({ top: document.getElementById("11").clientHeight - document.getElementById("13").clientHeight * 2 - document.getElementById("14").clientHeight - 190, left: 0, behavior: 'smooth' }), 10);
+                window.scrollTo({ top: document.getElementById("11").clientHeight - document.getElementById("13").clientHeight * 2 - document.getElementById("14").clientHeight - 90, left: 0, behavior: 'instant' });
+                setTimeout(window.scrollTo({ top: document.getElementById("11").clientHeight - document.getElementById("13").clientHeight * 2 - document.getElementById("14").clientHeight - 190, left: 0, behavior: 'smooth' }), 100);
             }
         }
     }
