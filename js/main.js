95f6cdd3411e271167ed47c75c7f480314bb6b07
RahatLukum13 <karmazin2007@mail.ru>
2025-01-21 20:15:14 +0300

    a

--- js/main.js
+++ js/main.js
@@ -14,11 +14,11 @@ if('ontouchstart' in window || navigator.maxTouchPoints){
         if(flag == true){
             if(document.getElementById("11").clientHeight - document.getElementById("13").clientHeight - document.getElementById("14").clientHeight - 45 <= pageYOffset){
                 window.scrollTo(0, document.getElementById("13").clientHeight * 2 + 75);
-                window.scrollTo({ top: document.getElementById("13").clientHeight * 2 + 175, left: 0, behavior: 'smooth' });
+                setTimeout(window.scrollTo({ top: document.getElementById("13").clientHeight * 2 + 175, left: 0, behavior: 'smooth' }),10);
             }
             else if(document.getElementById("13").clientHeight + 30 >= pageYOffset){
                 window.scrollTo(0, document.getElementById("11").clientHeight - document.getElementById("13").clientHeight * 2 - document.getElementById("14").clientHeight - 90);
-                window.scrollTo({ top: document.getElementById("11").clientHeight - document.getElementById("13").clientHeight * 2 - document.getElementById("14").clientHeight - 190, left: 0, behavior: 'smooth' });
+                setTimeout(window.scrollTo({ top: document.getElementById("11").clientHeight - document.getElementById("13").clientHeight * 2 - document.getElementById("14").clientHeight - 190, left: 0, behavior: 'smooth' }), 10);
             }
         }
     }
