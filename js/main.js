670189e655b245526823407561f5a3fc6a6080b4
RahatLukum13 <karmazin2007@mail.ru>
2025-01-22 19:40:57 +0300

    a

--- js/main.js
+++ js/main.js
@@ -7,18 +7,16 @@ for(const element of document.getElementsByClassName("heart")){
         element.src="https://karmazinwlad.github.io/Oktyabr/img/img1.png";
     }
 }
-console.log(2);
+console.log(1);
 if('ontouchstart' in window || navigator.maxTouchPoints){
     var flag = true;
     function scrolling(){
         // if(flag == true){
             if(document.getElementById("11").clientHeight - document.getElementById("13").clientHeight - document.getElementById("14").clientHeight - 45 <= pageYOffset){
-                window.scrollTo({ top: document.getElementById("13").clientHeight * 2 + 75, left: 0, behavior: 'instant' });
-                setTimeout(() => {window.scrollTo({ top: document.getElementById("13").clientHeight * 2 + 175, left: 0, behavior: 'smooth' })}, 100);
+                window.scrollTo({ top: document.getElementById("13").clientHeight * 2 + 475, left: 0, behavior: 'smooth' });
             }
             else if(document.getElementById("13").clientHeight + 30 >= pageYOffset){
-                window.scrollTo({ top: document.getElementById("11").clientHeight - document.getElementById("13").clientHeight * 2 - document.getElementById("14").clientHeight - 90, left: 0, behavior: 'instant' });
-                setTimeout(() => {window.scrollTo({ top: document.getElementById("11").clientHeight - document.getElementById("13").clientHeight * 2 - document.getElementById("14").clientHeight - 190, left: 0, behavior: 'smooth' })}, 100);
+                window.scrollTo({ top: document.getElementById("11").clientHeight - document.getElementById("13").clientHeight * 2 - document.getElementById("14").clientHeight - 490, left: 0, behavior: 'smooth' });
             }
         // }
     }
